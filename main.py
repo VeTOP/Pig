db96ca1f85b55c7a1e0fc68ad3aca1990ed206bd
VeTOP <102178559+VeTOP@users.noreply.github.com>
2022-12-25 11:57:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,21 +10,7 @@ class Player:
         return hand
 
 
-print("""
-.----------------.  .----------------.  .----------------. 
-| .--------------. || .--------------. || .--------------. |
-| |   ______     | || |     _____    | || |    ______    | |
-| |  |_   __ \   | || |    |_   _|   | || |  .' ___  |   | |
-| |    | |__) |  | || |      | |     | || | / .'   \_|   | |
-| |    |  ___/   | || |      | |     | || | | |    ____  | |
-| |   _| |_      | || |     _| |_    | || | \ `.___]  _| | |
-| |  |_____|     | || |    |_____|   | || |  `._____.'   | |
-| |              | || |              | || |              | |
-| '--------------' || '--------------' || '--------------' |
- '----------------'  '----------------'  '----------------' """)
-
-
-# Asks how many players are there and puts them in a list called player_list
+# Make player list
 number_of_players = int(input("How many players? "))
 player_list = []
 for i in range(number_of_players):
@@ -32,28 +18,30 @@ for i in range(number_of_players):
     player_list.append(p)
 
 
-# Iter though the player list
-p_num = 1
+
+#player = Player()
 for player in player_list:
-    print("It's Player", p_num, "'s turn.")
-    # For every player, play the round
-    m_score = 0
-    while True:
-        print("Player", p_num, "throws the die and scores:")
+    throwing = True
+    score = 0
+    while throwing:
         hand = player.throw()
-
         print(hand)
-        if hand == 1:
-            print("and loses all his points!")
-            break
-        else:
-            m_score += hand
-            con = input("Continue?(Y/N) ")
-            if con.lower() == "y":
-                continue
-            else:
-                break
-    player.score += m_score
-    print("Score:", player.score)
-    p_num+=1
 
+        if hand != 1:
+            score += hand
+            if score >= 10:
+                print("you win")
+                throwing = False
+                break
+            else:
+
+                con = input("y/n")
+                if con != "y":
+                    throwing = False
+                continue
+
+        else:
+            score = 0
+            throwing = False
+    player.score += score
+    print(player.score)
